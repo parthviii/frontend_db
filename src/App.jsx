af53c41a7ab67dee1e06174c66db50c9271aeb7b
Nishi <62937652+nishim21@users.noreply.github.com>
2023-08-08 18:17:24 +0530

    Update data fetching in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,106 +6,7 @@ import Header from "./components/Header";
 import TableComponent from "./components/TableComponent";
 import { Container, Row, Col } from "react-bootstrap";
 
-// const data = [
-//   {
-//     select: "",
-//     id: "1",
-//     isin: "US9128283C60",
-//     cusip: "9128283C6",
-//     issuer: "United States Department of the Treasury",
-//     maturitydate: "2023-08-07",
-//     coupon: "1.3",
-//     type: "Goverment Bond",
-//     facevalue: "1000.0",
-//     status: "Inactive",
-//   },
-//   {
-//     id: "2",
-//     isin: "US9128283C60",
-//     cusip: "9128283C6",
-//     issuer: "United States Department of the Treasury",
-//     maturitydate: "2021-08-07",
-//     coupon: "1.3",
-//     type: "Goverment Bond",
-//     facevalue: "1000.0",
-//     status: "Active",
-//   },
-//   {
-//     id: "3",
-//     isin: "US9128283C60",
-//     cusip: "9128283C6",
-//     issuer: "United States Department of the Treasury",
-//     maturitydate: "2020-08-07",
-//     coupon: "1.3",
-//     type: "Goverment Bond",
-//     facevalue: "1000.0",
-//     status: "Inactive",
-//   },
-
-//   {
-//     id: "4",
-//     isin: "US9128283C60",
-//     cusip: "9128283C6",
-//     issuer: "United States Department of the Treasury",
-//     maturitydate: "2025-08-07",
-//     coupon: "1.3",
-//     type: "Goverment Bond",
-//     facevalue: "1000.0",
-//     status: "Active",
-//   },
-
-//   {
-//     select: "",
-//     id: "5",
-//     isin: "US9128283C60",
-//     cusip: "9128283C6",
-//     issuer: "United States Department of the Treasury",
-//     maturitydate: "2030-08-07",
-//     coupon: "1.3",
-//     type: "Goverment Bond",
-//     facevalue: "1000.0",
-
-//     status: "Inactive",
-//   },
-//   {
-//     id: "6",
-//     isin: "US9128283C60",
-//     cusip: "9128283C6",
-//     issuer: "United States Department of the Treasury",
-//     maturitydate: "2032-08-07",
-//     coupon: "1.3",
-//     type: "Goverment Bond",
-//     facevalue: "1000.0",
-//     status: "Active",
-//   },
-//   {
-//     id: "7",
-//     isin: "US9128283C60",
-//     cusip: "9128283C6",
-//     issuer: "United States Department of the Treasury",
-//     maturitydate: "2028-08-07",
-//     coupon: "1.3",
-//     type: "Goverment Bond",
-//     facevalue: "1000.0",
-//     status: "Inactive",
-//   },
-
-//   {
-//     id: "demoid126783648376",
-//     isin: "US9128283C60",
-//     cusip: "9128283C6",
-//     issuer: "United States Department of the Treasury",
-//     maturitydate: "2024-08-07",
-//     coupon: "1.3",
-//     type: "Goverment Bond",
-//     facevalue: "1000.0",
-//     status: "Active",
-//   },
-
-//   // Add more data objects here
-// ];
-
-const data = [
+/* const data = [
   {
     id: 0,
     isin: "US9128283C60",
@@ -166,23 +67,24 @@ const data = [
     status: "active",
     isfav: 0,
   },
-];
-const App = () => {
-  // const [data,setData]=useState([]);
+]; */
 
-  // useEffect(() => {
-  // fetch("http://localhost:9090/bonds/security")
-  // .then((response) => response.json())
-  // .then((responseData) => {
-  //   setData(responseData);
-  //  // You can see the fetched data here
-  //   // Now you can store the data in a local variabl
-  //   // Do further processing with responseData
-  // })
-  // .catch((error) => {
-  //   console.error("Error fetching data:", error);
-  // });
-  // },[]);
+const App = () => {
+  const [data,setData]=useState([]);
+
+  useEffect(() => {
+  fetch("http://localhost:9090/bonds/security")
+  .then((response) => response.json())
+  .then((responseData) => {
+    setData(responseData);
+   // You can see the fetched data here
+    // Now you can store the data in a local variabl
+    // Do further processing with responseData
+  })
+  .catch((error) => {
+    console.error("Error fetching data:", error);
+  });
+  },[]);
 
   const [filter, setFilter] = useState("all"); // Initialize with default filter
   return (
