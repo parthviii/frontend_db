2c447f150bee99868ba449d4771066c49f90720e
nikhil gupta <guptanikhil898@gmail.com>
2023-08-08 18:37:02 +0530

    add login

--- src/App.jsx
+++ src/App.jsx
@@ -1,106 +1,15 @@
-import React, { useState, useEffect } from "react";
-import SearchBar from "./components/SearchBar";
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
-import Header from "./components/Header";
-import TableComponent from "./components/TableComponent";
-import { Container, Row, Col } from "react-bootstrap";
-
-/* const data = [
-  {
-    id: 0,
-    isin: "US9128283C60",
-    cusip: "9128283C6",
-    issuer: "United States Department of the Treasury",
-    maturitydate: "2023-08-07",
-    coupon: 1.3,
-    type: "Goverment Bond",
-    facevalue: 1000,
-    status: "active",
-    isfav: 0,
-  },
-  {
-    id: 1,
-    isin: "ISIN12345678",
-    cusip: "CUSIP1234",
-    issuer: "Apas pvt. ltd.",
-    maturitydate: "2025-12-31",
-    coupon: 0.05,
-    type: "Corporate Bond",
-    facevalue: 1000,
-    status: "active",
-    isfav: 0,
-  },
-  {
-    id: 2,
-    isin: "ISIN87654321",
-    cusip: "CUSIP4321",
-    issuer: "Goverment Bond",
-    maturitydate: "2030-06-30",
-    coupon: 0.03,
-    type: "Goverment Bond",
-    facevalue: 1000,
-    status: "active",
-    isfav: 0,
-  },
-  {
-    id: 3,
-    isin: "ISIN55555555",
-    cusip: "CUSIP7890",
-    issuer: "pop pvt. ltd.",
-    maturitydate: "2028-08-15",
-    coupon: 0.04,
-    type: "Corporate Bond",
-    facevalue: 1000,
-    status: "active",
-    isfav: 0,
-  },
-  {
-    id: 4,
-    isin: "ISIN88888888",
-    cusip: "CUSIP1010",
-    issuer: "Goverment Bond",
-    maturitydate: "2032-03-20",
-    coupon: 0.025,
-    type: "Goverment Bond",
-    facevalue: 1000,
-    status: "active",
-    isfav: 0,
-  },
-]; */
-
+import React, { useState,useEffect } from "react";
+import Login from "./components/Login";
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Dashboard from "./components/Dashboard";
 const App = () => {
-  const [data,setData]=useState([]);
-
-  useEffect(() => {
-  fetch("http://localhost:9090/bonds/security")
-  .then((response) => response.json())
-  .then((responseData) => {
-    setData(responseData);
-   // You can see the fetched data here
-    // Now you can store the data in a local variabl
-    // Do further processing with responseData
-  })
-  .catch((error) => {
-    console.error("Error fetching data:", error);
-  });
-  },[]);
-
-  const [filter, setFilter] = useState("all"); // Initialize with default filter
   return (
-    <Container fluid className="contain">
-      <Navbar />
-      <Header />
-      <Row>
-        <Col xs={12} md={2}>
-          <Sidebar setFilter={setFilter} />
-        </Col>
-        <Col xs={12} md={10}>
-          <SearchBar data={data} filter={filter} />
-          {/* Your content for the right two-thirds of the dashboard */}
-        </Col>
-      </Row>
-    </Container>
+    <BrowserRouter>
+    <Routes>
+   <Route path='/' element={<Login />} /> 
+  <Route path='/dashboard' element={<Dashboard/>} /> 
+  </Routes>
+  </BrowserRouter>
   );
 };
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,80 +1,91 @@
-import React, { useState } from 'react'
-import '../styles/Login.css'
-export default function Login() {   
+import React, { useState } from 'react';
+import '../styles/Login.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-    const handleSubmit=(event)=>{
-        event.preventDefault();
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:9090/bonds/user/login", {
+        email,
+        password,
+      });
+
+      if (response.data === "invalid") {
+        setError("Invalid email or password");
+        alert("Invalid Credentials! Please try again");
+       
+      } else {
+        const userRole = response.data.role;
+        navigate('/dashboard');
+      }
+    } catch (error) {
+      setError("Invalid email or password");
     }
-    const [email,setEmail]=useState("");
-    return (
-        <section class="vh-100">
-            <div class="container-fluid h-custom">
-                <div class="row d-flex justify-content-center align-items-center h-100">
-                    <div class="col-md-9 col-lg-6 col-xl-5">
-                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                            class="img-fluid" alt="Sample image" />
-                    </div>
-                    <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                        <form onSubmit={handleSubmit}>
-                            {/* <!-- Email input --> */}
-                            <div class="form-outline mb-4">
-                                <input type="email" id="form3Example3" class="form-control form-control-lg"
-                                    placeholder="Enter a valid email address" />
-                                <label class="form-label" for="form3Example3" onChange={(e) => setEmail(e.target.value)}>Email address</label>
-                            </div>
-                            {/* <!-- Password input --> */}
-                            <div class="form-outline mb-3">
-                                <input type="password" id="form3Example4" class="form-control form-control-lg"
-                                    placeholder="Enter password" />
-                                <label class="form-label" for="form3Example4">Password</label>
-                            </div>
-                            <div class="d-flex justify-content-between align-items-center">
-                                {/* <!-- Checkbox --> */}
-                                <div class="form-check mb-0">
-                                    <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
-                                    <label class="form-check-label" for="form2Example3">
-                                        Remember me
-                                    </label>
-                                </div>
-                                <a href="#!" class="text-body">Forgot password?</a>
-                            </div>
+  };
 
-                            <div class="text-center text-lg-start mt-4 pt-2">
-                                <button type="button" class="btn btn-primary btn-lg"
-                                    style={{paddingLeft: "2.5rem" , paddingRight: "2.5rem"}}>Login</button>
-                                <p class="small fw-bold mt-2 pt-1 mb-0">Don't have an account? <a href="#!"
-                                    class="link-danger">Register</a></p>
-                            </div>
-
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div
-                class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
-                {/* <!-- Copyright --> */}
-                <div class="text-white mb-3 mb-md-0">
-                    Copyright © 2024. All rights reserved.
-                </div>
-                {/* <!-- Copyright --> */}
-
-                {/* <!-- Right --> */}
-                <div>
-                    <a href="#!" class="text-white me-4">
-                        <i class="fab fa-facebook-f"></i>
-                    </a>
-                    <a href="#!" class="text-white me-4">
-                        <i class="fab fa-twitter"></i>
-                    </a>
-                    <a href="#!" class="text-white me-4">
-                        <i class="fab fa-google"></i>
-                    </a>
-                    <a href="#!" class="text-white">
-                        <i class="fab fa-linkedin-in"></i>
-                    </a>
-                </div>
-                {/* <!-- Right --> */}
-            </div>
-        </section>
-    )
+  return (
+    <section class="vh-100">
+      <div class="container-fluid h-custom">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-md-9 col-lg-6 col-xl-5">
+            <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+              class="img-fluid" alt="Sample image" />
+          </div>
+          <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+            <form onSubmit={handleSubmit}>
+              <div class="form-outline mb-4">
+                <input
+                  type="email"
+                  id="form3Example3"
+                  class="form-control form-control-lg"
+                  placeholder="Enter a valid email address"
+                  value={email}
+                  onChange={handleEmailChange} // Use separate change handler
+                />
+                <label class="form-label" for="form3Example3">
+                  Email address
+                </label>
+              </div>
+              <div class="form-outline mb-3">
+                <input
+                  type="password"
+                  id="form3Example4"
+                  class="form-control form-control-lg"
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={handlePasswordChange} // Use separate change handler
+                />
+                <label class="form-label" for="form3Example4">
+                  Password
+                </label>
+              </div>
+              {/* ... rest of the form ... */}
+              <button
+                type="submit"
+                class="btn btn-primary btn-lg"
+                style={{ paddingLeft: "2.5rem", paddingRight: "2.5rem" }}
+              >
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
