1933f5b47756f650bfffda115afe657d09fb696a
Prajjwal <prajjwalfire@gmail.com>
2023-08-08 23:02:42 +0530

    fixed the create

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,16 +26,16 @@ function DatePickerApp() {
 }
 const Header = () => {
   const [isFormOpen, setIsFormOpen] = useState(false);
-  const [ID, setID] = useState("");
-  const [Isin, setIsin] = useState("");
-  const [Cusip, setCusip] = useState("");
-  const [Type, setType] = useState("");
-  const [Issuer, setIssuer] = useState("");
+  const [id, setID] = useState("");
+  const [isin, setIsin] = useState("");
+  const [cusip, setCusip] = useState("");
+  const [type, setType] = useState("");
+  const [issuer, setIssuer] = useState("");
   const [maturitydate, setDate] = useState("");
   const [coupon, setCoupon] = useState("");
   const [facevalue, setFace] = useState("");
   const [items, setItems] = useState([]);
-  const [status, setStatus] = useState([]);
+  const [status, setStatus] = useState("Active");
   const [selectedDate, setSelectedDate] = useState(null);
 
   const openForm = () => {
@@ -57,34 +57,37 @@ const Header = () => {
 
   const handleSubmit = () => {
     const newItem = {
-      ID,
-      Isin,
-      Cusip,
-      Issuer,
+      id,
+      isin,
+      cusip,
+      issuer,
       maturitydate,
       coupon,
-      Type,
+      type,
       facevalue,
       status,
     };
-    setItems([...items,newItem]);
+    setItems([...items, newItem]);
     console.log(newItem);
     // Send data to backend
-    // fetch("http://localhost:5000/api/items", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify(newItem),
-    // })
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     console.log("Data sent to backend:", data);
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error sending data to backend:", error);
-    //   });
-      
+    fetch("http://localhost:9090/bonds/security/create", {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json, text/plain, */*',
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newItem)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Data sent to backend:", data);
+      })
+      .catch((error) => {
+        console.error("Error sending data to backend:", error);
+      });
+    //  .then(data => this.setState({ creditcards: data.creditcards }))
+    //  .catch(error => this.setState({ error }));
+
     closeForm();
   };
 
@@ -98,7 +101,7 @@ const Header = () => {
         noWrap
         component="div"
         sx={{ display: { xs: "none", sm: "block" } }}
-        style={{color: "#101010"}}
+        style={{ color: "#101010" }}
       >
         BondMate
       </Typography>
@@ -112,52 +115,52 @@ const Header = () => {
           Create Bond
         </Typography>
       </Button>
-      <Drawer open={isFormOpen} PaperProps={{style: {width: "600px", height: "100%", backgroundColor: "transparent"}}} anchor={"right"} onClose={closeForm} style={{ display: "flex", borderRadius: "12px 12px 0px 0px", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-        <Box display={"flex"} flexDirection={"row"} style={{borderRadius: "12px 0px 0px 0px", background: "linear-gradient(92.81deg, #003361 -6.17%, #007CEC 106.57%)", color: "#FFF", padding:"20px 30px", justifyContent:"space-between"}}> 
-          <Box display={"flex"} style={{flexDirection: "column"}}>
+      <Drawer open={isFormOpen} PaperProps={{ style: { width: "600px", height: "100%", backgroundColor: "transparent" } }} anchor={"right"} onClose={closeForm} style={{ display: "flex", borderRadius: "12px 12px 0px 0px", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+        <Box display={"flex"} flexDirection={"row"} style={{ borderRadius: "12px 0px 0px 0px", background: "linear-gradient(92.81deg, #003361 -6.17%, #007CEC 106.57%)", color: "#FFF", padding: "20px 30px", justifyContent: "space-between" }}>
+          <Box display={"flex"} style={{ flexDirection: "column" }}>
             <Typography variant="h5" fontWeight={"bold"}>Add Bond Details</Typography>
             <Typography variant="subtitle">Create a new bond</Typography>
           </Box>
-          <IconButton style={{color: "white", width: "20px", height: "20px", marginTop:"10px"}} onClick={closeForm}>
-              <CloseIcon />
+          <IconButton style={{ color: "white", width: "20px", height: "20px", marginTop: "10px" }} onClick={closeForm}>
+            <CloseIcon />
           </IconButton>
         </Box>
-        <Box style={{ display: "flex", flexDirection: "column", padding: "20px", backgroundColor: "#fff", borderRadius: "5px", height:"100%"  }}>
-          
-          <Box style={{padding: "10px 10px"}}>
-            <TextField width={"100%"} style={{fontSize: "14px", width: "100%"}} required label="ID" onChange={(e) => setID(e.target.value)} />
+        <Box style={{ display: "flex", flexDirection: "column", padding: "20px", backgroundColor: "#fff", borderRadius: "5px", height: "100%" }}>
+
+          <Box style={{ padding: "10px 10px" }}>
+            <TextField width={"100%"} style={{ fontSize: "14px", width: "100%" }} required label="ID" onChange={(e) => setID(e.target.value)} />
           </Box>
-          <Box style={{padding: "10px 10px"}}>
-            <TextField  style={{fontSize: "14px", width: "100%"}} required label="Isin" onChange={(e) => setIsin(e.target.value)} />
+          <Box style={{ padding: "10px 10px" }}>
+            <TextField style={{ fontSize: "14px", width: "100%" }} required label="Isin" onChange={(e) => setIsin(e.target.value)} />
           </Box>
-          <Box style={{padding: "10px 10px"}}>
-            <TextField  style={{fontSize: "14px", width: "100%"}} required label="Cusip" onChange={(e) => setCusip(e.target.value)} />
+          <Box style={{ padding: "10px 10px" }}>
+            <TextField style={{ fontSize: "14px", width: "100%" }} required label="Cusip" onChange={(e) => setCusip(e.target.value)} />
           </Box>
-          <Box style={{padding: "10px 10px"}}>
-            <TextField  style={{fontSize: "14px", width: "100%"}} required label="Issuer" onChange={(e) => setIssuer(e.target.value)} />
+          <Box style={{ padding: "10px 10px" }}>
+            <TextField style={{ fontSize: "14px", width: "100%" }} required label="Issuer" onChange={(e) => setIssuer(e.target.value)} />
           </Box>
-          <Box style={{padding: "10px 10px"}}>
-          <TextField
-            label="Date"
-            type="date"
-            value={maturitydate}
-            onChange={(e) => {
-              setDate(e.target.value)
-            }
-            }
-            InputLabelProps={{
-              shrink: true,
-            }}
-          />
-        </Box>
-          <Box style={{padding: "10px 10px"}}>
-            <TextField type= "number" step="any" style={{fontSize: "14px", width: "100%"}} required label="Coupon rate" onChange={(e) => setCoupon(e.target.value)} />
+          <Box style={{ padding: "10px 10px" }}>
+            <TextField
+              label="Date"
+              type="date"
+              value={maturitydate}
+              onChange={(e) => {
+                setDate(e.target.value)
+              }
+              }
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
           </Box>
-          <Box style={{padding: "10px 10px"}}>
-            <TextField  style={{fontSize: "14px", width: "100%"}} required label="Type" onChange={(e) => setType(e.target.value)} />
+          <Box style={{ padding: "10px 10px" }}>
+            <TextField type="number" step="any" style={{ fontSize: "14px", width: "100%" }} required label="Coupon rate" onChange={(e) => setCoupon(e.target.value)} />
           </Box>
-          <Box style={{padding: "10px 10px"}}>
-            <TextField type="number" step="any" style={{fontSize: "14px", width: "100%"}} required label="Face value" onChange={(e) => setFace(e.target.value)} />
+          <Box style={{ padding: "10px 10px" }}>
+            <TextField style={{ fontSize: "14px", width: "100%" }} required label="Type" onChange={(e) => setType(e.target.value)} />
+          </Box>
+          <Box style={{ padding: "10px 10px" }}>
+            <TextField type="number" step="any" style={{ fontSize: "14px", width: "100%" }} required label="Face value" onChange={(e) => setFace(e.target.value)} />
           </Box>
           <Button variant="contained" onClick={handleSubmit}>
             Save
