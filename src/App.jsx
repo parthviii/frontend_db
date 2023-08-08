cfef98437497069725f227aaf698302acf73e816
Nishi Mahalwar <nishi.nitb@gmail.com>
2023-08-08 13:17:19 +0530

    Add search and sort to table data

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import SearchBar from "./components/SearchBar";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
-import { FlexRow } from "./components/Containers";
 import Header from "./components/Header";
 import TableComponent from "./components/TableComponent";
-import { Container, Row, Col, } from 'react-bootstrap';
+import { Container, Row, Col } from "react-bootstrap";
 
 const data = [
   {
-    select:"",
+    select: "",
     id: "1",
     isin: "US9128283C60",
     cusip: "9128283C6",
@@ -18,89 +17,89 @@ const data = [
     coupon: "1.3",
     type: "Goverment Bond",
     facevalue: "1000.0",
-    status: "Inactive"
-},
-{
-  id: "2",
-  isin: "US9128283C60",
-  cusip: "9128283C6",
-  issuer: "United States Department of the Treasury",
-  maturitydate: "2023-08-07",
-  coupon: "1.3",
-  type: "Goverment Bond",
-  facevalue: "1000.0",
-  status: "Active"
-},
-{
-  id: "3",
-  isin: "US9128283C60",
-  cusip: "9128283C6",
-  issuer: "United States Department of the Treasury",
-  maturitydate: "2023-08-07",
-  coupon: "1.3",
-  type: "Goverment Bond",
-  facevalue: "1000.0",
-  status: "Inactive"
-},
-
-{
-  id: "4",
-  isin: "US9128283C60",
-  cusip: "9128283C6",
-  issuer: "United States Department of the Treasury",
-  maturitydate: "2023-08-07",
-  coupon: "1.3",
-  type: "Goverment Bond",
-  facevalue: "1000.0",
-  status: "Active"
-},
+    status: "Inactive",
+  },
+  {
+    id: "2",
+    isin: "US9128283C60",
+    cusip: "9128283C6",
+    issuer: "United States Department of the Treasury",
+    maturitydate: "2021-08-07",
+    coupon: "1.3",
+    type: "Goverment Bond",
+    facevalue: "1000.0",
+    status: "Active",
+  },
+  {
+    id: "3",
+    isin: "US9128283C60",
+    cusip: "9128283C6",
+    issuer: "United States Department of the Treasury",
+    maturitydate: "2020-08-07",
+    coupon: "1.3",
+    type: "Goverment Bond",
+    facevalue: "1000.0",
+    status: "Inactive",
+  },
 
   {
-    select:"",
+    id: "4",
+    isin: "US9128283C60",
+    cusip: "9128283C6",
+    issuer: "United States Department of the Treasury",
+    maturitydate: "2025-08-07",
+    coupon: "1.3",
+    type: "Goverment Bond",
+    facevalue: "1000.0",
+    status: "Active",
+  },
+
+  {
+    select: "",
     id: "5",
     isin: "US9128283C60",
     cusip: "9128283C6",
     issuer: "United States Department of the Treasury",
-    maturitydate: "2023-08-07",
+    maturitydate: "2030-08-07",
     coupon: "1.3",
     type: "Goverment Bond",
     facevalue: "1000.0",
-    status: "Inactive"
-},
-{
-  id: "6",
-  isin: "US9128283C60",
-  cusip: "9128283C6",
-  issuer: "United States Department of the Treasury",
-  maturitydate: "2023-08-07",
-  coupon: "1.3",
-  type: "Goverment Bond",
-  facevalue: "1000.0",
-  status: "Active"
-},
-{
-  id: "7",
-  isin: "US9128283C60",
-  cusip: "9128283C6",
-  issuer: "United States Department of the Treasury",
-  maturitydate: "2023-08-07",
-  coupon: "1.3",
-  type: "Goverment Bond",
-  facevalue: "1000.0",
-  status: "Inactive"
-},
+    status: "Inactive",
+  },
+  {
+    id: "6",
+    isin: "US9128283C60",
+    cusip: "9128283C6",
+    issuer: "United States Department of the Treasury",
+    maturitydate: "2032-08-07",
+    coupon: "1.3",
+    type: "Goverment Bond",
+    facevalue: "1000.0",
+    status: "Active",
+  },
+  {
+    id: "7",
+    isin: "US9128283C60",
+    cusip: "9128283C6",
+    issuer: "United States Department of the Treasury",
+    maturitydate: "2028-08-07",
+    coupon: "1.3",
+    type: "Goverment Bond",
+    facevalue: "1000.0",
+    status: "Inactive",
+  },
 
-{
-  id: "8",
-  isin: "US9128283C60",
-  cusip: "9128283C6",
-  issuer: "United States Department of the Treasury",
-  maturitydate: "2023-08-07",
-  coupon: "1.3",
-  type: "Goverment Bond",
-  facevalue: "1000.0",
-  status: "Active"
-}
+  {
+    id: "demoid126783648376",
+    isin: "US9128283C60",
+    cusip: "9128283C6",
+    issuer: "United States Department of the Treasury",
+    maturitydate: "2024-08-07",
+    coupon: "1.3",
+    type: "Goverment Bond",
+    facevalue: "1000.0",
+    status: "Active",
+  },
 
   // Add more data objects here
 ];
@@ -108,20 +107,18 @@ const data = [
 const App = () => {
   return (
     <Container fluid className="contain">
-    <Navbar/>
-    <Header/>
-  <Row>
-    <Col xs={12} md={2}>  
-      <Sidebar/>
-    </Col>
-    <Col xs={12} md={10}>
-    <SearchBar /> 
-      {/* Your content for the right two-thirds of the dashboard */}
-      <TableComponent data={data}/>
-    </Col> 
-   </Row>
-</Container>
-
+      <Navbar />
+      <Header />
+      <Row>
+        <Col xs={12} md={2}>
+          <Sidebar />
+        </Col>
+        <Col xs={12} md={10}>
+          <SearchBar data={data} />
+          {/* Your content for the right two-thirds of the dashboard */}
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,17 +4,22 @@ import { Box, Button, Typography } from "@mui/material";
 import { FlexRowAlignCenter, FlexRowSpaceBetween } from "./Containers";
 
 const Header = () => {
-
-
   return (
-    <FlexRowSpaceBetween  className="grad" style={{ padding: "1rem",backgroundColor: "#C2E6F4",marginBottom:"10px" }}>
+    <FlexRowSpaceBetween
+      className="grad"
+      style={{
+        padding: "1.3rem",
+        backgroundColor: "#C2E6F4",
+        marginBottom: "0.5rem",
+      }}
+    >
       <Typography
         variant="h4"
         noWrap
         component="div"
         sx={{ display: { xs: "none", sm: "block" } }}
       >
-        Mature Guard
+        BondMate
       </Typography>
       <Button size="small" variant="contained">
         <Typography
